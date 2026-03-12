30745e7ce80380a20b90aa615914b59cba54fc03
aidar <aidar1998@gmail.com>
2026-03-12 13:30:12 +0500

    removed junk

--- qtClient/src/main.cpp
+++ qtClient/src/main.cpp
@@ -27,18 +27,8 @@ void initWebRtcClient(QGuiApplication &app, QQmlApplicationEngine &engine) {
   initVideoPlayers(app, engine, client);
 }
 
-int main(int argc, char *argv[]) {
-    std::vector<int> arr = {1, 2, 3, 4, 5};
-    std::vector<std::byte> arr2;
-
-    auto *st = reinterpret_cast<std::byte *>(arr.data());
-    int size = arr.size() * sizeof(int);
-
-    arr2.insert(arr2.end(), st, st + size);
-    for (int i = 0; i < arr2.size(); i++) {
-        std::cout << std::to_integer<int>(arr2[i]) << std::endl;
-    }
-
+int main(int argc, char *argv[])
+{
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
